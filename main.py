b3ed803f5b2e2b81681c28c2d38aa55f639c7f63
Maktub108 <e.kozulin@yandex.ru>
2025-03-16 10:28:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def hello(name):
     print( f"привет {name}")
-hello(input("как дела?"))
-print("хорошо")
+    hello(input("как дела?"))
+    print("хорошо")
+
+def summa(a,b):
+    return  a + b
+def null(a,b):
+    return  a * b
